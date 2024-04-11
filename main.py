2bc0df216493604add5d52cd8f56307ba44a3f41
josestrada123 <mariajosefinaestrada1@gmail.com>
2024-04-11 14:39:50 -0400

    Josefina changes the decoder

--- main.py
+++ main.py
@@ -15,8 +15,24 @@ def encoder(num):
         encoded_list.append(encrypted_num_string)
     return "".join(encoded_list)
 
+def decoder(num):
+    num = list(num)
+    decoded_list=[]
+    for term in list(num):
+        if int(term) == 2:
+            decoded_list.append("9")
+        elif int(term) == 1:
+            decoded_list.append("8")
+        elif int(term) == 0:
+            decoded_list.append("7")
+        else:
+            decoded_list.append(str(int(term)-3))
+    return "".join(decoded_list)
+
+
 
 if __name__ == "__main__":
+    encoded = " "
     while True:
         print("Menu\n"
               "-------------\n"
@@ -27,9 +43,9 @@ if __name__ == "__main__":
 
         if option == 1:
             password = input("Please enter your password to encode: ")
-            encoder(password)
+            encoded = encoder(password)
             print("Your password has been encoded and stored!\n")
         elif option == 2:
-            pass
+            print("Your encoded password is", encoded,"and the original password is", decoder(encoded))
         elif option == 3:
             break
